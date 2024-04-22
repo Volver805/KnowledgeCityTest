d1b1be0d327c52ca166ed95e788a54bf23b1c3d6
Ahmed Sayed <ahmed.sayed@zid.sa>
2024-04-22 17:30:20 +0200

    update courses and category services to link courses with parent categories

--- api/services/CategoryService.php
+++ api/services/CategoryService.php
@@ -2,9 +2,28 @@
 
 namespace Api\Services;
 
+use PDO;
+
 require_once 'services/BaseService.php';
 
 class CategoryService extends BaseService
 {
+    protected $coursesTable = 'courses';
+
+    public function getAll()
+    {
+
+        $stmt = $this->db->prepare("SELECT c1.*, COALESCE(COUNT($this->coursesTable.category_id), 0) AS course_count
+        FROM $this->table AS c1 
+        LEFT JOIN $this->coursesTable AS courses ON courses.category_id = c1.id OR courses.category_id IN (
+            SELECT id FROM $this->table WHERE parent_id = c1.id OR parent_id IN (
+                SELECT id FROM $this->table WHERE parent_id = c1.id OR parent_id IN (
+                        SELECT id FROM $this->table WHERE parent_id = c1.id
+                    )
+            )
+        ) GROUP BY c1.id;");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
     protected string $table = 'categories';
 }
--- api/services/CourseService.php
+++ api/services/CourseService.php
@@ -9,10 +9,21 @@ class CourseService extends BaseService
 {
 
     protected string $table = 'courses';
+    protected string $categoriesTable = 'categories';
+
     public function getCoursesByCategory(string $categoryId): array
     {
-        $stmt = $this->db->prepare("SELECT * FROM $this->table WHERE category_id = ?");
-        $stmt->execute([$categoryId]);
+        $stmt = $this->db->prepare("
+        SELECT * FROM $this->table WHERE category_id IN (
+            SELECT id from $this->categoriesTable WHERE id=:categoryId 
+            UNION SELECT id from $this->categoriesTable WHERE parent_id = :categoryId
+            UNION SELECT id from $this->categoriesTable WHERE parent_id IN (
+                SELECT id from $this->categoriesTable WHERE parent_id = :categoryId) 
+            UNION SELECT id from $this->categoriesTable where parent_id IN (
+                SELECT id from $this->categoriesTable WHERE parent_id IN (
+                    SELECT id from $this->categoriesTable WHERE parent_id = :categoryId)))
+        ");
+        $stmt->execute(['categoryId' => $categoryId]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
